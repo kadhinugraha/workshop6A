051eecd8baff150b4b3edd6adb4dcea0d0a844f6
Kiki Adhinugraha <k.adhinugraha@latrobe.edu.au>
2022-08-15 10:07:52 +1000

    fixed bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
   // Respond with "hello world" for requests that hit our root "/"       
   
   app.get("/", function (req, res) 
-  {      return res.send("Hello World";  });  
+  {      return res.send("Hello World");  });  
   
   // listen to port 8000 by default     
   app.listen(process.env.PORT || 8000, () => 
